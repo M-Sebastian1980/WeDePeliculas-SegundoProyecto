746c7b363520c293588c2db22dd297d3ffdb12ba
Aldana Oyola <lizaldanaoyolaruiz@gmail.com>
2026-03-22 20:24:55 -0300

    funcion de validacion del form completa

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,12 @@
+import { useEffect } from "react"
+import { SubirUsuarios } from "../data/user"
+import { useNavigate } from "react-router"
+
 function LoginPage (){
 
+    const navigate = useNavigate()
+
+    function handleLogin (e){ 
         e.preventDefault() 
         const form = e.target 
         const formData = new FormData(form) 
@@ -7,5 +14,27 @@ function LoginPage (){
         console.log(data) 
         const dataFromLS = JSON.parse(localStorage.getItem("users")) 
         const userFound = dataFromLS.find(user => user.email === data.email)
+        if (!userFound){ 
+            alert("Usuario No Encontrado") 
+            delete userFound.password
+            localStorage.setItem("auth",JSON.stringify({
+            auth: true,
+            ...userFound
+            }))
+            return 
+        } 
+        const passwordMatch = data.password === userFound.password 
+        if(!passwordMatch){ 
+            alert("Contraseña Incorrecta") 
+            return 
+        } 
+        alert("Usuario Logueado")
+        delete userFound.password
+        localStorage.setItem("auth",JSON.stringify({
+            auth: true,
+            ...userFound
+        }))
+        useNavigate("/admin")     
+    }
 
 }
