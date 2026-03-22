31bf3368d359fee525f4f57be95ca96997714229
Aldana Oyola <lizaldanaoyolaruiz@gmail.com>
2026-03-22 20:18:38 -0300

    validacion del formulario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,11 @@
 function LoginPage (){
-    
+
+        e.preventDefault() 
+        const form = e.target 
+        const formData = new FormData(form) 
+        const data = Object.fromEntries(formData) 
+        console.log(data) 
+        const dataFromLS = JSON.parse(localStorage.getItem("users")) 
+        const userFound = dataFromLS.find(user => user.email === data.email)
+
 }
