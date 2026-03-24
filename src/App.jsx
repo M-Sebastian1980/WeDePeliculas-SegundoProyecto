42639ac32e2be31a2f43d4dda05fd1358b30ccb8
Octavio <octaviofernandez40999@gmail.com>
2026-03-24 00:22:57 -0300

    css del footer agregado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import { LoginPage } from './pages/Login'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/hero' element={<Hero />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
