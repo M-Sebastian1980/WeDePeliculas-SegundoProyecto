2cbe3daad9936452f0a1a449b33235eefba52caa
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-03-25 01:28:04 -0300

    refactor: cambio numeros de padding y margin dentro del css de admin

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ModalMovie from "../components/Modal";
 import ModalDeleteMovie from "../components/ModalDeleteMovie";
+import Navbar from "../components/Navbar";
 import "../styles/admin.css";
 
 function AdminPage() {
@@ -17,36 +18,40 @@ function AdminPage() {
     }, []);
 
     return (
-        <div className="admin-container">
-            <div className="admin-header">
-                <h1 className="admin-title">Panel de Administración</h1>
-                <ModalMovie
-                    buttonName='Agregar película'
-                    getMovies={getMovies}
-                />
-            </div>
 
-            <table className="admin-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Película</th>
-                        <th>Categoría</th>
-                        <th>Descripción</th>
-                        <th>Publicado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        movies.map((movie) => (
-                            <tr key={movie.id}>
-                                <td>{movie.id}</td>
-                                <td>{movie.name}</td>
-                                <td>{movie.category}</td>
-                                <td>{movie.description}</td>
-                                <td>{movie.published}</td>
-                                <td>
+        <>
+
+            <Navbar />
+
+            <div className="admin-container">
+                <div className="admin-header">
+                    <h1 className="admin-title">Panel de Administración</h1>
+                    <ModalMovie
+                        buttonName='Agregar película'
+                        getMovies={getMovies}
+                    />
+                </div>
+
+                <table className="admin-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Película</th>
+                            <th>Categoría</th>
+                            <th>Descripción</th>
+                            <th>Publicado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            movies.map((movie) => (
+                                <tr key={movie.id}>
+                                    <td>{movie.id}</td>
+                                    <td>{movie.name}</td>
+                                    <td>{movie.category}</td>
+                                    <td>{movie.description}</td>
+                                    <td>{movie.published ? "Sí" : "No"}</td>
                                     <td>
                                         <div className="admin-actions">
                                             <ModalMovie
@@ -60,14 +65,14 @@ function AdminPage() {
                                             />
                                         </div>
                                     </td>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
 
-        </div>
+            </div>
+        </>
     )
 }
 export default AdminPage;
