95c2c5f481cdaccb962977db6d8019f3eded4d38
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-03-25 00:25:16 -0300

    fix: arreglo el path de movieDetail, por las dudas

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/hero' element={<Hero />} />
+        <Route element={<ProductDetail />} path='/product/:id'/>
+        <Route element={<ProtectedRoutes/>}>
+          <Route element={<AdminPage/>} path='/admin'/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
