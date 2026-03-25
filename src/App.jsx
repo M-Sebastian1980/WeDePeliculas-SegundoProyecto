8eea5dd525416c14c71e97de029c54e95c26d4f0
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-03-25 00:34:11 -0300

    fix: arreglo problema con ProtectedRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import { LoginPage } from './pages/Login'
 import Hero from './components/Hero'
+import AdminPage from './pages/Admin'
+import ProtectedRoutes from './components/ProtectedRoutes'
 
 function App() {
   return (
@@ -10,9 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/hero' element={<Hero />} />
-        <Route element={<ProductDetail />} path='/product/:id'/>
         <Route element={<ProtectedRoutes/>}>
-          <Route element={<AdminPage/>} path='/admin'/>
+          <Route path='/admin' element={<AdminPage/>} />
         </Route>
       </Routes>
     </BrowserRouter>
